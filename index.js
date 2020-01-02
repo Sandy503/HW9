1ef1dce1a4040315567cd611ac205a183f23c654
Kyle Sandoval <kdsandoval2@gmail.com>
2020-01-01 20:10:28 -0800

    Added questions

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-const questions = [
-  
-];
+const inquirer = require("inquirer");
+const fs = require('fs');
+const ghtml = require("../HW9/generateHTML");
+
+inquirer.prompt([
+    {
+        type: "list",
+        message: "What is your favorite color?",
+        name: "color",
+        choices: [
+          "green",
+          "blue",
+          "pink",
+          "red"
+        ]
+      },
+    {
+      type: "input",
+      name: "name",
+      message: "What is your Github username?"
+    }   
+  ]).then(function(data) {
+      
+  });
+
 
 function writeToFile(fileName, data) {
  
