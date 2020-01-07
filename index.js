91977bd9e13a825bdbef8ab875baa0729617fd62
Kyle Sandoval <kdsandoval2@gmail.com>
2020-01-07 12:34:37 -0800

    Added function to write newProfile.html

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
-const ghtml = require("../HW9/generateHTML");
+const generateHTML = require("./generateHTML");
 const axios = require('axios')
 const util = require("util");
 const electron = require("electron")
-const electronToHTML = require("electron-to-html")
+const electronToHTML = require("electron-html-to")
 const writeFileAsync = util.promisify(fs.writeFile);
 const readFileAsync = util.promisify(fs.readFile);
 
-inquirer.prompt([
+questions = () => {
+  return inquirer.prompt([
     {
         type: "list",
         message: "What is your favorite color?",
@@ -25,19 +26,22 @@ inquirer.prompt([
       name: "username",
       message: "What is your Github username?"
     }   
-  ]).then(function({ username }) {
-    const queryUrl = `https://api.github.com/users/${username}`;
-    axios.get(queryUrl).then(function(res) {
-        console.log(res.data)
+  ]).then(function(data) {
+    const queryUrl = `https://api.github.com/users/${data.username}`;
+    const gitStars = `https://api.github.com/users/${data.username}/starred`;
+    axios.get(queryUrl).then( (res) => {
+      axios.get(gitStars).then( (stars) => {
+
+        html = generateHTML(data, res, stars);
+          writeFileAsync("newProfile.html", html);
+    
+        }).then( () => {
+          readFileAsync("newProfile.html", "utf8")
+    
         });
-  });
-
-
-function writeToFile(fileName, data) {
- 
-}
-
-function init() {
-
-};
-init();
+    
+         });
+         
+      })
+    };
+questions();
